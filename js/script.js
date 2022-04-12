ec7ff139115af2879f267a6dbfc7259023a8a7c1
knuckelstr <micaelds243@gmail.com>
2022-04-12 17:58:26 -0300

    botões funcionado , caso vc coloque um operator ele vai para a variavel 2 , caso n tenha ele continua na variavel 1, calculadora perto do final

--- js/script.js
+++ js/script.js
@@ -1,56 +1,169 @@
-var n1 = 3;
-var n2 = 2;
-var operator = "+";
+var n1 
+var n2 
+var operator
 
-const numbers = () => {
+
+const Operators = () => {
+
+    document.querySelectorAll('.operator')[0].onclick = function() {
+      return operator = document.querySelector('p').innerText += "+"
+    }
+ 
+    document.querySelectorAll('.operator')[1].onclick = function() {
+     return operator = document.querySelector('p').innerText += "-"
+ }
+ 
+ document.querySelectorAll('.operator')[2].onclick = function() {
+     return operator = document.querySelector('p').innerText += "/"
+ }
+ 
+ document.querySelectorAll('.operator')[3].onclick = function() {
+     return operator = document.querySelector('p').innerText += "*"
+ }
+ 
+ }
+ 
+ Operators()
+
+
+
+
     
 document.querySelectorAll(".number")[0].onclick = function() {
-    document.querySelector('p').innerText += 1 
+
+    if(operator == "") {
+        var n1 = document.querySelector('p').innerText += 1 
+
+        return n1   
+        
+    } else {
+        return n2 = document.querySelector('p').innerText += 1  
+
+    }
+    
 };
 
+
+    
+    
+ 
+
+
 document.querySelectorAll(".number")[1].onclick = function() {
-    document.querySelector('p').innerText += 2
+    if(operator == "") {
+        var n1 = document.querySelector('p').innerText += 2 
+        return console.log( Number(n1) )
+    } else {
+        var n2 = document.querySelector('p').innerText += 2
+        return console.log(  Number(n2) )      
+    }
+    
+    
 };
 
 document.querySelectorAll(".number")[2].onclick = function() {
-    document.querySelector('p').innerText += 3
+    if(operator == "") {
+        var n1 = document.querySelector('p').innerText += 3 
+        return console.log( Number(n1) )
+    } else {
+        var n2 = document.querySelector('p').innerText += 3 
+        return console.log(  Number(n2) )      
+    }
+    
 };
 
 document.querySelectorAll(".number")[3].onclick = function() {
-    document.querySelector('p').innerText += 4
+    if(operator == "") {
+        var n1 = document.querySelector('p').innerText += 4 
+        return console.log( Number(n1) )
+    } else {
+        var n2 = document.querySelector('p').innerText += 4 
+        return console.log(  Number(n2) )      
+    }
+    
 };
 
 document.querySelectorAll(".number")[4].onclick = function() {
-    document.querySelector('p').innerText += 5
+    if(operator == "") {
+        var n1 = document.querySelector('p').innerText += 5 
+        return console.log( Number(n1) )
+    } else {
+        var n2 = document.querySelector('p').innerText += 5 
+        return console.log(  Number(n2) )      
+    }
+    
 };
 
 document.querySelectorAll(".number")[5].onclick = function() {
-    document.querySelector('p').innerText += 6
+    if(operator == "") {
+        var n1 = document.querySelector('p').innerText += 6 
+        return console.log( Number(n1) )
+    } else {
+        var n2 = document.querySelector('p').innerText += 6 
+        return console.log(  Number(n2) )      
+    }
+    
 };
 
 document.querySelectorAll(".number")[6].onclick = function() {
-    document.querySelector('p').innerText += 7
+    if(operator == "") {
+        var n1 = document.querySelector('p').innerText += 7 
+        return console.log( Number(n1) )
+    } else {
+        var n2 = document.querySelector('p').innerText += 7 
+        return console.log(  Number(n2) )      
+    }
+    
 };
 
 document.querySelectorAll(".number")[7].onclick = function() {
-    document.querySelector('p').innerText += 8
+    if(operator == "") {
+        var n1 = document.querySelector('p').innerText += 8 
+        return console.log( Number(n1) )
+    } else {
+        var n2 = document.querySelector('p').innerText += 8 
+        return console.log(  Number(n2) )      
+    }
+    
 };
 
 document.querySelectorAll(".number")[8].onclick = function() {
-    document.querySelector('p').innerText += 9
+    if(operator == "") {
+        var n1 = document.querySelector('p').innerText += 9 
+        return console.log( Number(n1) )
+    } else {
+        var n2 = document.querySelector('p').innerText += 9 
+        return console.log(  Number(n2) )      
+    }
+    
 };
 
 document.querySelectorAll(".number")[9].onclick = function() {
-    document.querySelector('p').innerText += 0
+    if(operator == "") {
+        var n1 = document.querySelector('p').innerText += 0 
+        return console.log( Number(n1) )
+    } else {
+        var n2 = document.querySelector('p').innerText += 0 
+        return console.log(  Number(n2) )      
+    }
+  
+    
 };
 
-document.querySelectorAll('.OperatorRed')[2].onclick = function() {
-    document.querySelector('p').innerHTML = ""
-}
 
-}
 
-numbers()
+ // Botão Apagar
+
+
+
+
+
+
+
+
+
+
+
 
 const operatorFilter = (operator,n1,n2) => {
     if(operator === "+") {
@@ -66,6 +179,7 @@ const operatorFilter = (operator,n1,n2) => {
         return console.log(n1 * n2)
     }
 }
+
 
 
 
